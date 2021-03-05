0903f3bddfd4d8ac5af72de7c9479e5adeb044b7
Vaibhav <vaibhav.s@simformsolutions.com>
2021-03-05 19:11:10 +0530

    Error Babel Setting

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import x from "./nav";
+// import { x } from "./nav";
+// const x = 
+// import {x} from "./nav"
+// const x = require('./nav');
+console.log(x);
---
+++ src/nav.js
@@ -0,0 +1,8 @@
+// const x ="simform";
+// export default x ="simform";
+// export default function sum(x,y){
+//     return x + y;
+// };
+let x = "simform";
+export default x;
+// module.exports = {x};
